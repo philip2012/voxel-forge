d869bd3421af383d7309547ee64171a17419a5de
Philips Nguyen <nvananphuc2022@gmail.com>
2026-05-30 21:09:04 +0700

    Add voxel constants and block type definitions

---
+++ Assets/_Project/Scripts/Voxels/BlockType.cs
@@ -0,0 +1,12 @@
+// file: Assets/_Project/Scripts/Voxels/BlockType.cs
+
+public enum BlockType: byte
+{
+    Air = 0,
+    Bedrock = 1,
+    Stone = 2,
+    Dirt = 3,
+    Grass = 4,
+    Wood = 5,
+    Leaves = 6,
+}
---
+++ Assets/_Project/Scripts/Voxels/VoxelData.cs
@@ -0,0 +1,46 @@
+// file: Assets/_Project/Scripts/Voxels/VoxelData.cs
+using UnityEngine;
+
+public static class VoxelData
+{
+    public const int ChunkWidth = 16;
+    public const int ChunkHeight = 16;
+
+    // Total number of block slots inside one chunk.
+    public const int ChunkVoxelCount = ChunkWidth * ChunkHeight * ChunkWidth;
+
+    // formula to convert 3d block into a index for 1d array
+    // index = x + ChunkWidth * (y + ChunkHeight * z)
+    
+    public static readonly Vector3[] VoxelVertices = new Vector3[]
+    {
+        new Vector3(0, 0, 0),
+        new Vector3(1, 0, 0),
+        new Vector3(1, 1, 0),
+        new Vector3(0, 1, 0),
+        new Vector3(0, 0, 1),
+        new Vector3(1, 0, 1),
+        new Vector3(1, 1, 1),
+        new Vector3(0, 1, 1),
+    };
+
+    public static readonly Vector3Int[] FaceChecks = new Vector3Int[]
+    {
+        new Vector3Int(0, 0, -1),
+        new Vector3Int(0, 0, 1),
+        new Vector3Int(0, 1, 0),
+        new Vector3Int(0, -1, 0),
+        new Vector3Int(-1, 0, 0),
+        new Vector3Int(1, 0, 0),
+    };
+
+    public static readonly int[,] VoxelTriangles = new int[6,4]
+    {
+        {0, 3, 1, 2},
+        {5, 6, 4, 7},
+        {3, 7, 2, 6},
+        {1, 5, 0, 4},
+        {4, 7, 0, 3},
+        {1, 2, 5, 6},
+    };
+}
