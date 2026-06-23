7ddc2234736b3f1e582df558a5b103fdeb25788a
Philips Nguyen <nvananphuc2022@gmail.com>
2026-06-23 21:19:13 +0700

    Add left click block breaking
    -right now it is intentionally unoptimized, will adjust later

---
+++ Assets/_Project/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private World world;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float interactionDistance = 12f;
+
+    private void Awake()
+    {
+        if (playerCamera == null)
+        {
+            playerCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            BreakBlock();
+        }
+    }
+
+    private void BreakBlock()
+    {
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out RaycastHit hit, interactionDistance))
+        {
+            Vector3 blockPoint = hit.point - hit.normal * 0.01f;
+            Vector3Int blockPosition = Vector3Int.FloorToInt(blockPoint);
+
+            world.SetBlock(blockPosition, BlockType.Air);
+        }
+    }
+}
